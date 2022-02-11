b57a22e129a78868c0ef4a35890336b8c95748c7
shafivulla339 <99337360+shafivulla339@users.noreply.github.com>
2022-02-11 16:06:32 +0530

    Update main.py

--- app/routers/main.py
+++ app/routers/main.py
@@ -16,7 +16,3 @@ def name():
     return PlainTextResponse(f'Hello {name}!')
 
 
-@router.get("/name/{specific_name}", include_in_schema=False)
-def specific_name():
-    """Have a more specific way to provide a name to be greeted for."""
-    return PlainTextResponse(f'Hello, specific {specific_name}!')
